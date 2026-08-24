ae8a7dbcc52e58c7dd359e27e5d36ef1c1903e1b
Sarah <delatorre.sarahh@gmail.com>
2026-08-24 14:54:01 -0300

    override

--- src/main/java/com/exemplo/model/Alergia.java
+++ src/main/java/com/exemplo/model/Alergia.java
@@ -28,9 +28,7 @@ public class Alergia {
 
     @Override
     public String toString() {
-        return "Alergia{" +
-                "idAlergia=" + idAlergia +
-                ", nomeAlergia='" + nomeAlergia + '\'' +
-                '}';
+        return "id Alergia: " + getIdAlergia() +
+                "Nome Alergia: " + getNomeAlergia() + "\n";
     }
 }
