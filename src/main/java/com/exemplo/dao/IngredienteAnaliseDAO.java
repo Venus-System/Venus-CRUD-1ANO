5fbd8a5050d9d19a374f0886b7b072b09d093f09
Gustavo De Andrade Herance <20260105-ieg@instituto.ad>
2026-09-13 15:30:42 -0300

    NomeIngrediente e IngredienteAnalise

--- src/main/java/com/exemplo/dao/IngredienteAnaliseDAO.java
+++ src/main/java/com/exemplo/dao/IngredienteAnaliseDAO.java
@@ -1,4 +1,84 @@
 package com.exemplo.dao;
 
+import com.exemplo.controller.ConexaoBanco;
+import com.exemplo.model.IngredienteAnalise;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class IngredienteAnaliseDAO {
+
+    public boolean cadastrarIngredienteAnalise(IngredienteAnalise ingredienteAnalise) throws SQLException {
+        String sql = "insert into ingrediente_analise(id_ingrediente) values (?)";
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+            pstmt.setInt(1, ingredienteAnalise.getIdIngrediente());
+            return pstmt.executeUpdate() > 0;
+        }
+    }
+
+    public ArrayList<IngredienteAnalise> read() throws SQLException {
+        String sql = "select * from ingrediente_analise order by id_ingrediente_analise";
+        ArrayList<IngredienteAnalise> ingredienteAnalise = new ArrayList<>();
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql);
+             ResultSet rset = pstmt.executeQuery()) {
+            while (rset.next()) {
+                IngredienteAnalise ia1 = new IngredienteAnalise(
+                        rset.getInt("id_ingrediente_analise"),
+                        rset.getInt("id_ingrediente")
+                );
+                ingredienteAnalise.add(ia1);
+            }
+        } return ingredienteAnalise;
+
+    }
+
+    public IngredienteAnalise readById(int id) throws SQLException {
+        String sql = "select * from ingrediente_analise where id_ingrediente_analise =?";
+        IngredienteAnalise ingredienteAnalise = null;
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+
+            try (ResultSet rset = pstmt.executeQuery()) {
+                if (rset.next()) {
+                    ingredienteAnalise = new IngredienteAnalise(
+                            rset.getInt("id_ingrediente_analise"),
+                            rset.getInt("id_ingrediente")
+                    );
+                }
+            }
+
+        } return ingredienteAnalise;
+    }
+
+    public int alterarValores(IngredienteAnalise ingredienteAnalise) throws SQLException {
+        String sql = "update ingrediente_analise set id_ingrediente = ?, id_ingrediente = ?";
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, ingredienteAnalise.getIdIngrediente());
+            pstmt.setInt(1, ingredienteAnalise.getIdIngrediente());
+
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public int deleteById(int id) throws SQLException {
+        String sql = "delete from ingrediente_analise where id_ingrediente_analise = ?";
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setInt(1,id);
+            return pstmt.executeUpdate();
+
+        }
+    }
 }
--- src/main/java/com/exemplo/dao/NomeIngredienteDAO.java
+++ src/main/java/com/exemplo/dao/NomeIngredienteDAO.java
@@ -1,4 +1,88 @@
 package com.exemplo.dao;
 
+import com.exemplo.controller.ConexaoBanco;
+import com.exemplo.model.NomeIngrediente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class NomeIngredienteDAO {
+
+    public boolean cadastrarNomeIngrediente(NomeIngrediente nomeIngrediente) throws SQLException {
+        String sql = "insert into nome_ingrediente(nome_ingrediente, id_ingrediente) values (?, ?)";
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+            pstmt.setString(1, nomeIngrediente.getNomeIngrediente());
+            pstmt.setInt(2, nomeIngrediente.getIdIngrediente());
+            return pstmt.executeUpdate() > 0;
+        }
+    }
+
+    public ArrayList<NomeIngrediente> read() throws SQLException {
+        String sql = "select * from nome_ingrediente order by id_nome";
+        ArrayList<NomeIngrediente> nomeIngrediente = new ArrayList<>();
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql);
+             ResultSet rset = pstmt.executeQuery()) {
+            while (rset.next()) {
+                NomeIngrediente ni1 = new NomeIngrediente(
+                        rset.getString("nome_ingrediente"),
+                        rset.getInt("id_ingrediente"),
+                        rset.getInt("id_nome")
+                );
+                nomeIngrediente.add(ni1);
+            }
+        } return nomeIngrediente;
+
+    }
+
+    public NomeIngrediente readById(int id) throws SQLException {
+        String sql = "select * from nome_ingrediente where id_nome =?";
+        NomeIngrediente nomeIngrediente = null;
+
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+
+            try (ResultSet rset = pstmt.executeQuery()) {
+                if (rset.next()) {
+                    nomeIngrediente = new NomeIngrediente(
+                            rset.getString("nome_ingrediente"),
+                            rset.getInt("id_ingrediente"),
+                            rset.getInt("id_nome")
+                    );
+                }
+            }
+
+        } return nomeIngrediente;
+    }
+
+    public int alterarValores(NomeIngrediente nomeIngrediente) throws SQLException {
+        String sql = "update nome_ingrediente set nome_ingrediente = ?, id_ingrediente = ?, id_ingrediente = ?";
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setString(1, nomeIngrediente.getNomeIngrediente());
+            pstmt.setInt(2, nomeIngrediente.getIdIngrediente());
+            pstmt.setInt(2, nomeIngrediente.getIdIngrediente());
+
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public int deleteById(int id) throws SQLException {
+        String sql = "delete from nome_ingrediente where id_nome = ?";
+        try (Connection cnn = new ConexaoBanco().conectar();
+             PreparedStatement pstmt = cnn.prepareStatement(sql)) {
+
+            pstmt.setInt(1,id);
+            return pstmt.executeUpdate();
+
+        }
+    }
 }
